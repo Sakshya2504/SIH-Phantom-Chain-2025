8c1295cfc39bcf0c9fa16f117b46c7d5e4f386c6
Sakshya2504 <singhkaserasakshya2504@gmail.com>
2025-09-29 17:14:14 +0530

    Sakshya-Task Completed

---
+++ Phantom_Chain/Backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json()); 
+
+app.get('/api/health', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
