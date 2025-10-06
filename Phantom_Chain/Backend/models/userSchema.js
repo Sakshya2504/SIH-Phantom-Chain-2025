6d1ffb3435b861730d3ecb289d9b08a03b798a81
Sakshya2504 <singhkaserasakshya2504@gmail.com>
2025-10-06 23:09:55 +0530

    Updates

---
+++ Phantom_Chain/Backend/models/userSchema.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            minlength: [5, "Name must be at least 5 characters long"],
+            maxlenght: [20, "Name must be at most 20 characters long"],
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [
+                /^[\w.-]+@iiti\.ac\.in$/,
+                "Email must be a valid @iiti.ac.in address",
+            ],
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: [6, "Password must be at least 6 characters long"],
+            maxlenght: [10, "Password must be at most 10 characters long"],
+        },
+        userphoto: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
--- Phantom_Chain/Backend/server.js
+++ Phantom_Chain/Backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { User } from './models/userSchema.js';
+import bcrypt from 'bcryptjs';
 
 const app = express();
 const port = 3000;
@@ -7,10 +10,50 @@ const port = 3000;
 app.use(cors());
 app.use(express.json()); 
 
+try{
+    await mongoose.connect('mongodb://localhost:27017/phantomchain/users');
+    console.log("Connected to MongoDB");
+}
+catch(error){
+    console.log("Error connecting to MongoDB:", error);
+    alert("Error connecting to MongoDB:", error);
+}
+
 app.get('/api/health', (req, res) => {
     res.send('Hello World!');
 });
 
+
+//Signup Route
+app.post('/api/signup', async(req,res)=>{
+    const {name, email , password, userphoto} = req.body;
+    if (password.length < 6 || password.length > 10) {
+        return res.status(400).json({ errors: ['Password must be between 6 and 10 characters long'] });
+    }
+
+    try{
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({errors: ['User with email already exists']});
+        }
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hashedPassword(password, saltRounds);
+
+        const newUser = new User({name,email,hashedPassword,userphoto});
+        await newUser.save();
+        res.status(201).json({message: 'User created successfully', user:
+            {name: newUser.name , email: newUser.email , password: hashedPassword , userphoto: newUser.userphoto}
+        });
+    }
+    catch (err) {
+        if (err.name === 'ValidationError') {
+            const messages = Object.values(err.errors).map(e => e.message);
+            return res.status(400).json({ errors: messages });
+        }
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
